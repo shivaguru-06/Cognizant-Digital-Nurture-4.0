b38bb70a4890f374c85c396af33f9b5ced1633a5
Shivaguru S <162468200+shivaguru-06@users.noreply.github.com>
2025-05-28 09:30:48 +0530

    13_Debugger.js

--- Module 1-Javascript Answers/13_Debugger.js
+++ Module 1-Javascript Answers/13_Debugger.js
@@ -1,2 +1,9 @@
-console.log("Submitting form with:", user);
-debugger; 
+Debugging and Testing
+console.log("Form submitted");
+
+fetch("https://mockapi.io/register", {
+  method: "POST",
+  body: JSON.stringify({ name: "Ajay" }),
+}).then(response => {
+  console.log("Status:", response.status);
+});
